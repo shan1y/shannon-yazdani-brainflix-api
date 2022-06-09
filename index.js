26e70db7a925432556241f290f5b6b8042a559d2
Shannon Yazdani <shannonyazdani@gmail.com>
2022-06-09 15:39:47 -0400

    added comments to index.js for readibility

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const cors = require("cors");
 
 const videos = require("./routes/videos");
 
+//parse incoming json request and puts parsed data in req
 app.use(express.json());
 app.use(cors());
 app.use(express.static("public"));
 
+//load router module in app
 app.use("/videos", videos);
 
 app.listen(PORT, () => {
